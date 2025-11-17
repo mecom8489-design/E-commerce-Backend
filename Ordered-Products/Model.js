9af874b1da961dc2773fc1ce795604bacd379d29
mecom8489 <mecom8489@gmail.com>
2025-11-17 12:11:49 +0530

    get all orders

--- Ordered-Products/Model.js
+++ Ordered-Products/Model.js
@@ -31,9 +31,39 @@ const Order = {
 
   // Get all orders
   getAll: async () => {
-    const [rows] = await db.query("SELECT * FROM orders ORDER BY created_at DESC");
+    const [rows] = await db.query(`
+      SELECT 
+        o.order_id,
+        o.user_id,
+        o.product_id,
+        o.quantity,
+        o.price_per_unit,
+        o.total_price,
+        o.shipping_name,
+        o.shipping_phone,
+        o.shipping_address,
+        o.payment_method,
+        o.payment_status,
+        o.order_status,
+        o.created_at,
+        o.updated_at,
+        p.name AS product_name,
+        p.price AS product_price,
+        p.rating AS product_rating,
+        p.discount AS product_discount,
+        p.description AS product_description,
+        p.category AS product_category,
+        p.stock AS product_stock,
+        p.image AS product_image
+      FROM orders o
+      JOIN products p ON o.product_id = p.id
+      ORDER BY o.created_at DESC
+  `);
     return rows;
   },
+
+
+
 
   // Get order by ID
   getById: async (user_id) => {
