d53f2725e126ca88d83c4733ee8d88a98499af7f
mecom8489 <mecom8489@gmail.com>
2025-11-17 13:13:30 +0530

    uuuuuuu

--- Exclusively-Available/Controller.js
+++ Exclusively-Available/Controller.js
@@ -1,15 +1,31 @@
 const Exclusively = require("./Model");
+const fs = require('fs');
+
+const getFullImageUrl = (req, imagePath) => {
+    if (!imagePath) return null;
+    if (typeof imagePath !== 'string') imagePath = String(imagePath);
+    return `${req.protocol}://${req.get('host')}/${imagePath.replace(/\\/g, '/')}`;
+  };
+
 
 // 📦 Get all orders
 exports.getAllOrders = async (req, res) => {
     try {
-        const data = await Exclusively.getAll(); // ✔ don't overwrite the model
-        return res.status(200).json(data);
+        const data = await Exclusively.getAll();
+
+        const dataWithUrl = data.map(item => ({
+            ...item,
+            image: `${req.protocol}://${req.get('host')}/${item.image}`
+        }));
+
+        return res.status(200).json(dataWithUrl);
     } catch (error) {
         console.error("Error fetching orders:", error);
         return res.status(500).json({ message: "Internal server error" });
     }
 };
+
+
 // 📦 Create new order
 exports.createOrder = async (req, res) => {
     try {
