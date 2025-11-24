df8a3358772dd6ed6e2352fdd19a5e0a6bb23be0
mecom8489 <mecom8489@gmail.com>
2025-11-24 12:30:39 +0530

    get review api

--- review/Controller.js
+++ review/Controller.js
@@ -23,9 +23,31 @@ exports.createReview = async (req, res) => {
       message: "Review added successfully",
       review_id: result.insertId,
     });
-
   } catch (error) {
     console.error("Error creating review:", error);
     return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+
+
+exports.getAllReviews = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 25;
+    const offset = (page - 1) * limit;
+
+    const results = await Review.getReviewsByProduct(offset, limit);
+
+    res.status(200).json({
+      page,
+      limit,
+      count: results.length,
+      reviews: results
+    });
+
+  } catch (err) {
+    console.error("Review fetch error:", err);
+    return res.status(500).json({ message: "Server error" });
   }
 };
