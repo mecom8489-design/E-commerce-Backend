207c2fa0bce32a434809a077b5ded86deaf29aaa
mecom8489-design <mecom8489@gmail.com>
2025-11-28 13:43:15 +0530

    Update Controller.js

--- Ordered-Products/Controller.js
+++ Ordered-Products/Controller.js
@@ -22,7 +22,7 @@ const getFullImageUrl = (req, imagePath) => {
 let apiInstance = new brevo.TransactionalEmailsApi();
 apiInstance.setApiKey(
   brevo.TransactionalEmailsApiApiKeys.apiKey,
-  process.env.BREVO_API_KEY
+"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"
 );
 
 
