6227deef85abd070698e9cd35282856b1bb63b52
mecom8489 <mecom8489@gmail.com>
2025-12-04 12:10:54 +0530

    feat: add order management API routes for creation, retrieval, cancellation, and delivery date updates.

--- Ordered-Products/route.js
+++ Ordered-Products/route.js
@@ -6,7 +6,7 @@ router.post("/create", orderController.createOrder);
 router.get("/all", orderController.getAllOrders);
 router.get("/:id", orderController.getOrderById);
 router.put("/cancel/:order_id", orderController.cancelOrder);
-router.put("/update-delivery/:order_id", orderController.updateDeliveryDate);
+router.put("/update-delivery-date/:order_id", orderController.updateDeliveryDate);
 
 
 module.exports = router;
