f987f18ff2fa876845a36a87e76b9d89d2b7ad76
mecom8489 <mecom8489@gmail.com>
2025-12-10 10:43:32 +0530

    feat: Add MySQL database connection configuration and update server to use environment variables for port and domain.

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
  
 const pool = mysql.createPool({
-  host:"localhost",
+  host:"nozomi.proxy.rlwy.net",
   user:"root",
-  password:"",
-  database: "ecommerce_backend",
-  port:3306,
+  password:"PkdkxwDsToqFWSrcILxtjodRugRCTKdJ",
+  database: "railway",
+  port:51397,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
