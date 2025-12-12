297a0550a80511d4169e0218b06b23353c5d540c
mecom8489 <mecom8489@gmail.com>
2025-12-12 14:36:04 +0530

    jwt added

--- Controllers/authControl/authController.js
+++ Controllers/authControl/authController.js
@@ -70,16 +70,16 @@ exports.login = async (req, res) => {
     if (!user) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
-    // const isValid = await User.comparePasswords(password, user.password);
-    // if (!isValid) {
-    //   return res.status(401).json({ message: "Invalid email or passwordddddddddddddddddddddd" });
-    // }
+    const isValid = await User.comparePasswords(password, user.password);
+    if (!isValid) {
+      return res.status(401).json({ message: "Invalid email or passwordddddddddddddddddddddd" });
+    }
 
-    // const token = generateToken({
-    //   id: user.id,
-    //   email: user.email,
-    //   role: user.role,
-    // });
+    const token = generateToken({
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    });
 
     res.status(200).json({
       message: "Login successful",
