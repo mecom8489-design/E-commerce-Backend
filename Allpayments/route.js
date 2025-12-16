2def95efee1284f5716ba3cc0679c9384aa5eec8
mecom8489 <mecom8489@gmail.com>
2025-12-16 15:27:13 +0530

    allpayments

--- Allpayments/route.js
+++ Allpayments/route.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const paymentsController = require("./controller");
 
-router.get("/all", paymentsController.getAllPayments);
+router.get("/allpayments", paymentsController.getAllPayments);
 
 module.exports = router;
